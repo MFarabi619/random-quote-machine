5946c8ba7b2b1d45c1e6e39fc928ce41fdbca76e
Mumtahin Farabi <mfarabi619@gmail.com>
2022-11-22 10:26:16 -0500

    Imported Bootstrap
    Added the following statements at the top of src/App.js
    - import Button from "react-bootstrap/Button";
    - import "bootstrap/dist/css/bootstrap.min.css";

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
