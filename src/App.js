829efc6129efe1ffce5260bffb718877b67e0cbb
Mumtahin Farabi <mfarabi619@gmail.com>
2022-11-25 17:14:43 -0500

    Background color change works
    Used the useState hook instead of classes to get the background to change color when the button is clicked

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import "./App.scss";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.scss";
+import React, { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("#e01919");
+  colorArr = [];
+  const handleChange = () => {
+    setColor("#e0da19");
+    console.log("handleChange function called");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header
+        className="App-header"
+        style={{ backgroundColor: `${color}`, color: `${color}` }}
+      >
         <p></p>
-        <Button>New quote</Button>
+        <Button onClick={handleChange}>New quote</Button>
       </header>
     </div>
   );
