ea5853cea253e6c013590876c35623d1a8cfab98
Mumtahin Farabi <mfarabi619@gmail.com>
2022-12-02 16:50:50 -0500

    Random quotes can be displayed
    Numbers are used for testing purposes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.scss";
 import React, { useState } from "react";
 import { colorArray } from "./colors";
+import { quoteArray } from "./quotes";
 
 function App() {
   const [color, setColor] = useState(colorArray[0]);
   const [quote, setQuote] = useState("This is some text to be displayed");
 
   const handleChange = () => {
-    setColor(colorArray[Math.floor(Math.random() * colorArray.length - 1)]);
-    setQuote("You don't get what you want, you get what you work for");
+    let randomColor =
+      colorArray[Math.floor(Math.random() * (colorArray.length - 1))];
+
+    setColor(randomColor);
+
+    let vari = Math.floor(Math.random() * (quoteArray.length - 1));
+    console.log(vari);
+    let randomQuote = quoteArray[vari];
+
+    setQuote(randomQuote);
     console.log("handleChange function called");
   };
 
@@ -22,7 +31,7 @@ function App() {
         style={{ backgroundColor: `${color}`, color: `${color}` }}
       >
         <div className="quote-box">
-          {quote}
+          <p className="quote-text">{quote}</p>
           <Button className="quote-button" onClick={handleChange}>
             New quote
           </Button>
---
+++ src/quotes.js
@@ -0,0 +1,3 @@
+var quoteArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+export { quoteArray };
